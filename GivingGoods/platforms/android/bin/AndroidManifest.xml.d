96a8d1d05a2c6beb261390e111207f267bd9bdfe
Stephanie Ge <stephiege@gmail.com>
2013-11-26 00:37:02 -0800

    Revert "Updates"
    This reverts commit 59a751666642811eb5194c68735990e276f32100.

--- GivingGoods/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/neilh/Desktop/givingGoods/GivingGoods/platforms/android/bin/AndroidManifest.xml : \
-/Users/neilh/Desktop/givingGoods/GivingGoods/platforms/android/AndroidManifest.xml \
--- GivingGoods/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/neilh/Desktop/givingGoods/GivingGoods/platforms/android/bin/HelloWorld-debug-unaligned.apk : \
-/Users/neilh/Desktop/givingGoods/GivingGoods/platforms/android/bin/HelloWorld.ap_ \
-/Users/neilh/Desktop/givingGoods/GivingGoods/platforms/android/bin/classes.dex \
-/Users/neilh/Desktop/givingGoods/GivingGoods/platforms/android/libs/cordova-3.1.0.jar \
