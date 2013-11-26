3291dfb07ecfd00dce8189f3eb98fa0f79cbbb71
Stephanie Ge <stephiege@gmail.com>
2013-11-26 03:24:05 -0800

    dup

--- GivingGoods/www/js/camera.js
+++ GivingGoods/www/js/camera.js
@@ -56,9 +56,9 @@ var pictureSource;   // picture source
     //
     function capturePhoto() {
       // Take picture using device camera and retrieve image as base64-encoded string
-    //  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
-     //   destinationType: destinationType.DATA_URL });
-     navigator.camera.getPicture(uploadPhoto,null,{sourceType:1, quality:60});
+      //navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+       // destinationType: destinationType.DATA_URL });
+      navigator.camera.getPicture(uploadPhoto, null, {sourceType:1,  quality: 50});
     }
 
 
